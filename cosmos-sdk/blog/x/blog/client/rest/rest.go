38402533a0f91b4a36edc0226cc5db1b3dbae0d3
youngqqcn <youngqqcn@163.com>
2020-10-27 14:48:47 +0800

    add listPost http GET

--- cosmos-sdk/blog/x/blog/client/rest/rest.go
+++ cosmos-sdk/blog/x/blog/client/rest/rest.go
@@ -8,5 +8,9 @@ import (
 
 // RegisterRoutes registers blog-related REST handlers to a router
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-  // this line is used by starport scaffolding # 1
+	// this line is used by starport scaffolding # 1
+	//   r.HandleFunc("/blog/comment", listCommentHandler(cliCtx, "blog")).Methods("GET")
+	//   r.HandleFunc("/blog/comment", createCommentHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/blog/post", listPostHandler(cliCtx, "blog")).Methods("GET")
+	// r.HandleFunc("/blog/post", createPostHandler(cliCtx)).Methods("POST")
 }
